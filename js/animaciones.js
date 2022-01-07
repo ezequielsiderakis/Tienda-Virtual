328a361b23c05569198e84f1d6592108e1d40663
Ezequiel Siderakis <92487681+ezequielsiderakis@users.noreply.github.com>
2022-01-07 12:40:26 -0300

    Update animaciones.js

--- js/animaciones.js
+++ js/animaciones.js
@@ -1,6 +1,10 @@
 $("#escudo").animate({
     "font-size" : "50px"
   }).fadeOut(4000).delay(2500).fadeIn(4000)
+
+$("#div1").animate({
+    "font-size" : "40px"
+  }).slideUp(5500).delay(2500).fadeIn(5000)
   
   
   
